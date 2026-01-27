3b821ad560059d7796b00c246596f19d941d20e6
Rudy <s60990340@gmail.com>
2026-01-27 23:09:13 +0800

    FronendProducts update to button

--- src/layout/FrontendLayout.jsx
+++ src/layout/FrontendLayout.jsx
@@ -1,13 +1,33 @@
-import { Outlet } from "react-router";
+import { Outlet, Link } from "react-router";
 
 const FrontendLayout = () => {
   return (
     <>
-      <header></header>
+      <header>
+        <ul className="nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">
+              首頁
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/products">
+              產品列表
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/cart">
+              購物車
+            </Link>
+          </li>
+        </ul>
+      </header>
       <main>
         <Outlet />
       </main>
-      <footer></footer>
+      <footer>
+        <p>© 2026 RudyWu</p>
+      </footer>
     </>
   );
 };
--- src/views/front/Products.jsx
+++ src/views/front/Products.jsx
@@ -1,7 +1,54 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+const API_BASE = import.meta.env.VITE_API_BASE;
+const API_PATH = import.meta.env.VITE_API_PATH;
+
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(`${API_BASE}/api/${API_PATH}/products`);
+        setProducts(res.data.products);
+      } catch (error) {
+        console.log(error.response);
+      }
+    };
+    getProducts();
+  }, []);
+
   return (
     <>
-      <h1>前台產品列表</h1>
+      <div className="container">
+        <div className="row">
+          {products.map((product) => (
+            <div className="col-md-4 mb-3" key={product.id}>
+              <div className="card ">
+                <img
+                  src={product.imageUrl}
+                  className="card-img-top"
+                  alt={product.title}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{product.title}</h5>
+                  <p className="card-text">{product.description}</p>
+                  <p className="card-text">
+                    <del>原價：{product.origin_price}</del> 售價：
+                    {product.price}
+                    <small className="text-body-secondary">
+                      /{product.unit}
+                    </small>
+                  </p>
+                  <button type="button" className="btn btn-primary">
+                    查看更多
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
